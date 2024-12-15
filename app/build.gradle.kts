8cd68ecbc67f09ed0210540b15ea72f0f378a213
Marçal <marcal.asbo@gmail.com>
2024-12-15 23:03:58 +0100

    create firebase configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,28 @@
+import java.util.Properties
+
 plugins {
 	alias(libs.plugins.android.application)
 	alias(libs.plugins.kotlin.android)
 	alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.crashlytics)
+    alias(libs.plugins.google.services)
 }
 
 android {
 	namespace = "com.maasbodev.puntsenlinia"
+
+    signingConfigs {
+        create("release") {
+            val keystoreFile = project.rootProject.file("local.properties")
+            val properties = Properties()
+            properties.load(keystoreFile.inputStream())
+            keyAlias = properties.getProperty("KEYSTORE_KEY_ALIAS")
+            storeFile = file(properties.getProperty("KEYSTORE_STORE_FILE"))
+            storePassword = properties.getProperty("KEYSTORE_STORE_PASSWORD")
+            keyPassword = properties.getProperty("KEYSTORE_KEY_PASSWORD")
+        }
+    }
+
 	compileSdk = 34
 
 	defaultConfig {
@@ -19,12 +36,25 @@ android {
 	}
 
 	buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
 		release {
-			isMinifyEnabled = false
+            isDebuggable = false
+			isMinifyEnabled = false // TODO: Change to true
+			isShrinkResources = false // TODO: Change to true
 			proguardFiles(
 				getDefaultProguardFile("proguard-android-optimize.txt"),
 				"proguard-rules.pro"
 			)
+            signingConfig = signingConfigs.getByName("release")
 		}
 	}
 	compileOptions {
@@ -40,7 +70,6 @@ android {
 }
 
 dependencies {
-
 	implementation(libs.androidx.core.ktx)
 	implementation(libs.androidx.lifecycle.runtime.ktx)
 	implementation(libs.androidx.activity.compose)
@@ -49,11 +78,19 @@ dependencies {
 	implementation(libs.androidx.ui.graphics)
 	implementation(libs.androidx.ui.tooling.preview)
 	implementation(libs.androidx.material3)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.remote.config)
+    implementation(libs.firebase.crashlytics)
+
 	testImplementation(libs.junit)
+
 	androidTestImplementation(libs.androidx.junit)
 	androidTestImplementation(libs.androidx.espresso.core)
 	androidTestImplementation(platform(libs.androidx.compose.bom))
 	androidTestImplementation(libs.androidx.ui.test.junit4)
-	debugImplementation(libs.androidx.ui.tooling)
+
+    debugImplementation(libs.androidx.ui.tooling)
 	debugImplementation(libs.androidx.ui.test.manifest)
 }
