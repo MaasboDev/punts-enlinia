2762d9cd8b14da8ef27ea6e989844284176f5d11
Marçal <marcal.asbo@gmail.com>
2024-12-16 12:11:59 +0100

    create a pre commit file

--- README.md
+++ README.md
@@ -6,7 +6,12 @@
 
 This application is a helper for the people following the program [Punts Enlínia](https://www.puntsenlinia.com/).
 
+## How to copy hooks
+--------------------
+
+You automatically copy the hooks when you run the app.
+
 ## License
--------
+----------
 
 This project is licensed under the Apache License 2.0. See the [LICENSE file](LICENSE) for details.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
+import java.nio.file.attribute.PosixFilePermissions
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
@@ -6,10 +9,12 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
 }
 
 subprojects {
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
 
     ktlint {
         android.set(true)
@@ -21,5 +26,56 @@ subprojects {
         filter {
             exclude("**/generated/**")
         }
+    }
+}
+
+detekt {
+    val filesProp = project.findProperty("detektFiles") as String?
+    if (!filesProp.isNullOrBlank()) {
+        val fileList =
+            filesProp.split(",")
+                .filter { it.isNotBlank() }
+                .map { file(it) }
+        source.setFrom(fileList)
+    } else {
+        source.setFrom(files("src/main/kotlin"))
+    }
+}
+
+tasks.register("copyGitHooks") {
+    doLast {
+        val sourceDir = file("hooks")
+        val targetDir = file(".git/hooks")
+
+        sourceDir.listFiles()
+            ?.forEach { sourceFile ->
+                val targetFile = File(targetDir, sourceFile.name)
+                if (!targetFile.exists() ||
+                    Files.mismatch(sourceFile.toPath(), targetFile.toPath()) != -1L
+                ) {
+                    Files.copy(
+                        sourceFile.toPath(),
+                        targetFile.toPath(),
+                        StandardCopyOption.REPLACE_EXISTING,
+                    )
+                    print("> Copied hook: ${sourceFile.name}")
+                }
+            }
+    }
+
+    doLast {
+        file(".git/hooks/").walk()
+            .forEach { file ->
+                if (file.isFile) {
+                    try {
+                        Files.setPosixFilePermissions(
+                            file.toPath(),
+                            PosixFilePermissions.fromString("rwxr-xr-x"),
+                        )
+                    } catch (_: UnsupportedOperationException) {
+                        logger.warn("Unable to set POSIX permissions on ${file.name}.")
+                    }
+                }
+            }
     }
 }
