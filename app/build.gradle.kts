a86b53b4db7fa7ff5409f4f4a4056a621be7ee1a
Marçal <marcal.asbo@gmail.com>
2025-01-25 22:15:43 +0100

    stringresources dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,7 @@ android {
 
 dependencies {
     implementation(projects.core.designSystem)
+    implementation(projects.core.stringResources)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
